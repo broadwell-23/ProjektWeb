08535a32110f27537f186a173737d38359e05a46
broadwell-23 <farfarizky@live.com>
2017-04-17 19:41:08 +0700

    Revert "Voyager is installed"
    This reverts commit 6eb8f88ad8eccc025cb0135a498ece4ea40698f3.

--- config/app.php
+++ config/app.php
@@ -167,7 +167,6 @@ return [
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
-        TCG\Voyager\VoyagerServiceProvider::class,
 
         /*
          * Application Service Providers...
